135f46e54058c6dc11b51989bb6e8b991dea9df6
HIMANSHUTHAKUR00 <101318398+HIMANSHUTHAKUR00@users.noreply.github.com>
2024-09-07 23:22:16 +0530

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -24,7 +24,7 @@ const social = [
     {
         name: 'GitHub',
         icon: 'github',
-        link: 'https://github.com/once-ui-system/nextjs-starter',
+        link: 'https://github.com/HIMANSHUTHAKUR00',
     },
     {
         name: 'Instagram',
